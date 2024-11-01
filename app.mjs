cc6b641e8a6e2220844b24c2f0e4d21d2fe3f956
Jsinger03 <julian.singer@gmail.com>
2024-11-01 17:26:37 -0400

    made skeleton dashboard and completed base skeleton of express application in app.mjs

--- app.mjs
+++ app.mjs
@@ -9,13 +9,14 @@ const __dirname = path.dirname(__filename);
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'hbs');
 app.use((req, res, next) => {
     console.log(req.method, req.path, req.body);
     next();
 })
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('dashboard');//currently just a skeleton
 })
 
 app.listen(process.env.PORT ?? 3000);
